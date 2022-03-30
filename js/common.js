569a689b763e2e0daff820b0955b63b64066752f
Soobeom2 <95343781+Soobeom2@users.noreply.github.com>
2022-03-30 10:33:16 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -2,7 +2,33 @@ const elHeader = document.querySelector('header')
 const elVisual = document.querySelector('main .visual_wrap')
 const elHamburger = document.querySelector('header .trigger-menu')
 const elPopup = document.querySelector('.header_menu_box_btn')
-// const elUp_li = document.querySelectorAll('main .visual_wrap ul li')
+
+const backToTop = document.getElementById('backtotop');
+
+const checkScroll=()=>{
+
+    let pageOffset = window.pageYOffset;
+
+
+    if(pageOffset !== 0){
+
+        backToTop.classList.add('show');  
+    }else{
+        backToTop.classList.remove('show'); 
+    }
+}
+
+const moveBackToTop=()=>{
+    if(window.pageYOffset > 0 ){
+    
+        window.scrollTo({top:0, behavior:"smooth"});
+    }
+}
+
+
+window.addEventListener('scroll', checkScroll);
+
+backToTop.addEventListener('click',moveBackToTop);
 
 let num = 0;
 
--- js/contact.js
+++ js/contact.js
@@ -1,11 +1,6 @@
 const elUp_li = document.querySelectorAll('main .visual_wrap li')
 const elUp_btn = document.querySelectorAll('main .visual_wrap .Contact_button')
-const elCh_btn = document.querySelector('main .checkbox_btn')
 
-
-elCh_btn.onclick=function(){
-    this.classList.toggle('active')
-}
 
 window.addEventListener('scroll',function(){
     elUp_li.forEach(function(p){
@@ -18,7 +13,7 @@ window.addEventListener('scroll',function(){
 window.addEventListener('scroll',function(){
     elUp_btn.forEach(function(p){
             if(p.offsetTop - (window.innerHeight*0.8) < window.scrollY){
-            p.classList.add('active');
+            p.classList.add('active')
         }
     })
 })
